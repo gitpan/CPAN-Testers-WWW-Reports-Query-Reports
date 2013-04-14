f078d0d0c8fba685328bd949019f5cff85ea5249
Barbie <barbie@missbarbell.co.uk>
2013-04-14 13:16:27 +0000

    Import of BARBIE/CPAN-Testers-WWW-Reports-Query-Reports-0.07 from CPAN.
    gitpan-cpan-distribution: CPAN-Testers-WWW-Reports-Query-Reports
    gitpan-cpan-version:      0.07
    gitpan-cpan-path:         BARBIE/CPAN-Testers-WWW-Reports-Query-Reports-0.07.tar.gz
    gitpan-cpan-author:       BARBIE
    gitpan-cpan-maturity:     released

--- lib/CPAN/Testers/WWW/Reports/Query/Reports.pm
+++ lib/CPAN/Testers/WWW/Reports/Query/Reports.pm
@@ -3,7 +3,7 @@ package CPAN::Testers::WWW::Reports::Query::Reports;
 use strict;
 use warnings;
 
-our $VERSION = '0.06';
+our $VERSION = '0.07';
  
 #----------------------------------------------------------------------------
 
@@ -203,7 +203,7 @@ There are no known bugs at the time of this release. However, if you spot a
 bug or are experiencing difficulties, that is not explained within the POD
 documentation, please send bug reports and patches to the RT Queue (see below).
 
-Fixes are dependant upon their severity and my availablity. Should a fix not
+Fixes are dependent upon their severity and my availability. Should a fix not
 be forthcoming, please feel free to (politely) remind me.
 
 RT Queue -
@@ -227,7 +227,7 @@ Initially released during the 2012 QA Hackathon in Paris.
 
 =head1 COPYRIGHT AND LICENSE
 
-  Copyright (C) 2012 Barbie for Miss Barbell Productions.
+  Copyright (C) 2012-2013 Barbie for Miss Barbell Productions.
 
   This module is free software; you can redistribute it and/or
   modify it under the Artistic License 2.0.
